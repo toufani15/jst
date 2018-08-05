993e0f584f0bd89542fa93d006679d9eb971298e
toufani15 <toufani1515@gmail.com>
2018-08-05 10:49:01 +0800

    versi 1.0

---
+++ basic_jst.py
@@ -0,0 +1,18 @@
+x_inp = [1.0, 2.0, 3.0]
+y_inp = [2.0, 4.0, 6.0]
+
+w = 1.0
+
+def forward(x):
+    return x * w
+
+def loss(x,y):
+    y_pred = forward(x)
+    return (y_pred - y) * (y_pred - y)
+
+def grad(x,y):
+    return 2*x*(x*w - y)
+
+for epoch in range(10):
+    for x_val, y_val in zip(x_inp, y_inp):
+        print(x_val)
